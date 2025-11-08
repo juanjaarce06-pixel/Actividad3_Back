435708418cfa8306ef497a04a474180766e47888
juanjaarce06-pixel <juanja.arce06@gmail.com>
2025-11-08 10:00:28 -0500

    Delete app directory

--- app/inference/utils.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image
-from io import BytesIO
-def load_image(b: bytes) -> Image.Image:
-    return Image.open(BytesIO(b)).convert("RGB")
--- app/settings.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-import os
-class Settings(BaseModel):
-    model_bucket: str = os.getenv("MODEL_BUCKET", "")
-    model_manifest: str = os.getenv("MODEL_MANIFEST", "")
-    google_project: str = os.getenv("GOOGLE_CLOUD_PROJECT", "")
-    allow_origins: list[str] = ["*"]
-settings = Settings()
